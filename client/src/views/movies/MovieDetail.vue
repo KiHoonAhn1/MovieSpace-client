fe50a218b025a1a365da29388534c6e452ac3020
hyeinkim1305 <haileyyy1305@gmail.com>
2021-05-23 20:28:18 +0900

    review_create,detail_router

--- client/src/views/movies/MovieDetail.vue
+++ client/src/views/movies/MovieDetail.vue
@@ -5,10 +5,10 @@
         <iframe id="player" type="text/html" width="640" height="360" :src="youtubeVideoSrc" frameborder="0"></iframe>
       </div>
       <div class="p-4">
-        <div v-for="video in similarVideo" v-bind:key="video.id" class="d-flex">
+        <h3>비슷한 영화</h3>
+        <div v-for="video in similarVideo" v-bind:key="video.id" class="d-flex" @click="toDetail(video)">
           <img :src="getImage(video.poster_path)" style="width:80px; height:100px;">
           <div>{{ video.title }}</div>
-          
         </div>
         <div>
           <h2>{{ movie.title }}</h2>
@@ -19,24 +19,28 @@
     </div>
     <div>
       <div>
+        <input type="number" v-model.trim="score">
+        <input type="text" v-model.trim="content" @keyup.enter="createReview">
         <p>리뷰 리스트</p>
       </div>
     </div>
-    <!-- <h5>{{ movie }}</h5> -->
   </div>
 </template>
 
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
 import axios from 'axios'
+// const SERVER_URL = process.env.VUE_APP_SERVER_URL
 export default {
   name: 'MovieDetail',
   data: function () {
     return {
       movie: this.$route.params.movie,
       movieVideo: '',
-      // similarVideos: [],
       similarVideo: [],
+      reviewList: [],
+      content: '',
+      score: '',
     }
   },
   created: function () {
@@ -50,6 +54,8 @@ export default {
   },
   // 추가함
   created: async function () {
+
+    // detail 영화 정보 불러오기
     const videoId = this.movie.id
     const apiKey = '7a8df8004890b70d9d4175ce5a47331d'
     const VIDEO_URL = `https://api.themoviedb.org/3/movie/${videoId}/videos?api_key=${apiKey}&language=ko` 
@@ -59,6 +65,7 @@ export default {
       this.movieVideo = response.data.results[0].key
     })
 
+    // similar 영화 정보 불러오기
     const SIMILAR_URL = `https://api.themoviedb.org/3/movie/${videoId}/similar?api_key=${apiKey}&language=ko&page=1`
     axios.get(SIMILAR_URL)
     .then(response=> {
@@ -69,13 +76,10 @@ export default {
         }
         i ++
       }
-
-      // this.similarVideo.push(response.data.results[0])
-      // this.similarVideo.push(response.data.results[1])
-      // this.similarVideo.push(response.data.results[2])
-
-      console.log(this.similarVideo)
     })
+
+    // review list 불러오기
+    this.getReviews()
   },
   computed: {
     youtubeVideoSrc: function () {
@@ -83,8 +87,52 @@ export default {
     },
   },
   methods: {
+    setToken: function () {
+      const token = localStorage.getItem('jwt')
+      const config = {
+        Authorization: `JWT ${token}`
+      }
+      console.log(config)
+      return config
+    },
+    // similar movie 이미지 포스터 가져오기
     getImage: function (url) {
       return 'https://image.tmdb.org/t/p/original'+ url
+    },
+    // movie review list 불러오기
+    getReviews: function () {
+      axios({
+        method: 'get',
+        url: `http://127.0.0.1:8000/movies/${this.movie.id}/review/`,
+      })
+      .then((res)=> {
+        console.log(res.data)
+        this.reviewList = res.data
+      })
+    },
+
+    // 아래 코드들이 잘 작동이 안됨..
+    createReview: function() {
+      const reviewScore = {
+        content : this.content,
+        score : this.score,
+        user: '', 
+        movie: '',
+      }
+      console.log(reviewScore)
+      axios({
+        method: 'post',
+        url: `http://127.0.0.1:8000/movies/${this.movie.id}/review/`,
+        data: reviewScore,
+        headers: this.setToken()
+      })
+        .then((res)=> {
+          console.log(res)
+        })
+
+    },
+    toDetail : function () {
+      this.$router.push({name: 'MovieDetail', params: {movie:video}})
     }
   },
 }
