8e2e2991731c08d077ad77fcc6cdd9b54ae9420d
안기훈 <dksdmlwjd1@naver.com>
2021-05-26 14:00:59 +0900

    change navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,15 +8,17 @@
     <!-- 'bg-transparent'가 배경 투명하게 하는 것 -->
     <div id="nav" class="navbar navbar-expand-lg navbar-light bg-transparent">
       <div class="container-fluid">
-        <a class="navbar-brand" style="font-size:30px;" href="#">Navbar</a>
+        <router-link to="/" class="nav-link">
+          <a class="navbar-brand" style="font-size:30px;" href="#">
+            <img src="@/assets/rocket.jpg" alt="" style="width:30px; border-radius:15px;">
+            Movie Space
+          </a>
+        </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link">Home</router-link>
-            </li>
             <li class="nav-item">
               <router-link to="/movielist" class="nav-link">추천</router-link>
             </li>
